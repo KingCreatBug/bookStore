82e48c84b307814a893c7c17c63d57a5fa3cdfec
KingCreatBug <trunghoang.240500@gmail.com>
2025-03-19 15:51:28 +0700

    Chia Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login";
+import ContactPage from "./pages/contact";
+import BookPage from "./pages/book";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <h1>Not Found</h1>,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "contact",
+                element: <ContactPage />,
+            },
+            {
+                path: "book",
+                element: <BookPage />,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+]);
+
 function App() {
-    return <>Hello</>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
